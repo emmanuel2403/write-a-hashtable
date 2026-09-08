8e2b8186c1ecd9ef41c863a6e51dcc5233f6cd13
emmanuel2403 <ehurta11@asu.edu>
2026-09-07 19:48:30 -0700

    Delete item and delete hash table functions added

--- src/hash_table.c
+++ src/hash_table.c
@@ -19,4 +19,24 @@ ht_hash_table* ht_new(){
     return ht;
 }
 
+//Deletes a single key and value in the hash table
+static void ht_del_item(ht_item* i) {
+    free(i->key);
+    free(i->value);
+    free(i);
+}
+
+//Deletes the full hash table
+void ht_del_hash_table(ht_hash_table* ht){
+    for (int i = 0; i < ht->size; i++){
+        ht_item* item = ht->items[i];
+        if(item != NULL){
+            ht_del_item(item);
+        }
+    }
+
+    free(ht->items);
+    free(ht);
+}
+
 
